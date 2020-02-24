21a76347cae7116ea0a94d4a876ab76f6a1aa65c
ganultap <reypatlunag@gmail.com>
2020-02-25 07:21:27 +0800

    first commit

--- register/index.php
+++ register/index.php
@@ -1,5 +1,5 @@
 <?php 
     include("includes/template/header.php");
-    include("includes/template/content2.php");
+    include("includes/template/content.php");
     include("includes/template/footer.php");
 ?>
